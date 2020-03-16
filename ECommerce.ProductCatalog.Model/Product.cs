bc73459448bb311fa3293606a759e423822a1f1e
Sahan Serasinghe <sahan.serasinghe@readify.net>
2020-03-17 08:16:48 +1030

    Add AddProduct Transaction

---
+++ ECommerce.ProductCatalog.Model/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ECommerce.ProductCatalog.Model
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ ECommerce.ProductCatalog/ServiceFabricProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerce.ProductCatalog.Model;
+using Microsoft.ServiceFabric.Data;
+
+namespace ECommerce.ProductCatalog
+{
+    class ServiceFabricProductRepository : IProductRepository
+    {
+        private readonly IReliableStateManager _stateManager;
+        public ServiceFabricProductRepository(IReliableStateManager stateManager)
+        {
+            _stateManager = stateManager;
+        }
+        public async Task AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
